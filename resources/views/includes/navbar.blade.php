ed413bd7f06a1ae71c619ffae985a2e1bb3e5621
aladino24 <aladinozulmar@gmail.com>
2023-09-21 00:27:04 +0700

    first commit

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,14 +1,14 @@
 <!-- resources/views/includes/navbar.blade.php -->
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-secondary">
     <div class="container">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-                <a class="nav-item nav-link active" href="/">Activity <span class="sr-only">(current)</span></a>
-                <a class="nav-item nav-link" href="/metode">Metode</a>
+                <a class="nav-item nav-link font-weight-bold text-white" href="/">Activity <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link font-weight-bold text-white" href="/metode">Metode</a>
             </div>
         </div>
     </div>
